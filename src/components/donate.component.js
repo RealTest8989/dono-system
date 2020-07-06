d17149d8d26ec252eee88647361cd2354b48705e
Lennart Irrgang <Irrgang.Lennart@gmail.com>
2020-07-06 17:06:19 +0200

    make it better

--- src/components/donate.component.js
+++ src/components/donate.component.js
@@ -107,7 +107,7 @@ export default class Donate extends Component {
 
     console.log(donation);
 
-    axios.post('https://donation-system1.herokuapp.com/donations/add', donation)
+    axios.post('https://donation-system1.herokuapp.com:' + process.env.PORT + '/donations/add', donation)
       .then(res => console.log(res.data));
 
   }
