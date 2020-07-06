835f44a08179e3adf103e6a98ab5da845143905c
Lennart Irrgang <Irrgang.Lennart@gmail.com>
2020-07-06 13:46:19 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,22 @@ const donationsRouter = require('./routes/donations');
 
 app.use('/donations', donationsRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('../build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+/*
 if (process.env.NODE_ENV === 'production'){
   app.use.express.static('/../build');
 }
+*/
 
 
 app.listen(port, () => {
