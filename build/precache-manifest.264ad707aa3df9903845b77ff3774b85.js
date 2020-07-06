549d875190163c1dc5f1e86a4833925da90744b0
Lennart Irrgang <Irrgang.Lennart@gmail.com>
2020-07-06 19:49:38 +0200

    make it better

---
+++ build/precache-manifest.264ad707aa3df9903845b77ff3774b85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488acc721aec39fd12b4031626df7520",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab0e251e123f02d3d878",
+    "url": "/static/css/main.e67b9200.chunk.css"
+  },
+  {
+    "revision": "2b1fce8c02af81be52e8",
+    "url": "/static/js/2.b6d6a82e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b6d6a82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0e251e123f02d3d878",
+    "url": "/static/js/main.d441b987.chunk.js"
+  },
+  {
+    "revision": "e96e1e41669850542108",
+    "url": "/static/js/runtime-main.61a423c7.js"
+  },
+  {
+    "revision": "e07cc1ae8a8dc17f01db3249a1bf750d",
+    "url": "/static/media/donation-alert.e07cc1ae.mp3"
+  },
+  {
+    "revision": "4c8a8d13c1b49cf848d4546220501a46",
+    "url": "/static/media/logo.4c8a8d13.png"
+  }
+]);
