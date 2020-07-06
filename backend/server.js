8fda9030e695f3025fc055348ed3d4a65d377b09
Lennart Irrgang <Irrgang.Lennart@gmail.com>
2020-07-06 22:24:37 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,6 @@ if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static(path.join(__dirname, '../build')));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-
 }
 
 /*
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Alertbox from "./components/alertbox.component";
 
 function App() {
   return (
-    <Router>
+    <Router basename={`${process.env.PUBLIC_URL}/`}>
       <Route path="/alertbox" component={Alertbox} />
       <Route path="/donate" component={Donate} />
     </Router>
