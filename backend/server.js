bce49a1cf55a79dd70ed3bd635e34e7c431ab94a
Lennart Irrgang <Irrgang.Lennart@gmail.com>
2020-07-06 22:20:58 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,12 @@ app.use('/donations', donationsRouter);
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static(path.join(__dirname, '../build')));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+
 }
 
 /*
